65eea8c4c847e1480e7ef1bfcc3baa557e50e7f1
Lawrence Tan <lawrence.tan@2359media.com>
2017-06-08 22:52:50 +0800

    First Commit

---
+++ BMV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMV
+//
+//  Created by Lawrence Tan on 8/6/17.
+//  Copyright © 2017 Lawrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
